081e2333cefc91696c426fff48915b86b6bfe9d6
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-03-24 10:57:24 +0100

    complex number tests without optional part

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -7,6 +7,8 @@ package tasks.adts
 
 object Ex1ComplexNumbers:
 
+  private case class ComplexType(re: Double, im: Double)
+
   trait ComplexADT:
     type Complex
     def complex(re: Double, im: Double): Complex
@@ -19,12 +21,15 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
+
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    opaque type Complex = ComplexType
+    def complex(re: Double, im: Double): Complex = ComplexType(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+      def im(): Double = complex.im
+      def sum(other: Complex): Complex = ComplexType(complex.re + other.re, complex.im + other.im)
+      def subtract(other: Complex): Complex = complex.sum(ComplexType(-other.re, -other.im))
+      def asString(): String = complex.re.toString + {
+        if (complex.im >= 0) then " + " else " - "
+      } + complex.im.toString + { if (complex.im != 0) then "i" else " "}
