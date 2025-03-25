16814cdc5e9f608e5a5efbd4dfeb9004bb1e3cb9
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-03-25 11:01:13 +0100

    feat: testEmptyStack

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -1,7 +1,12 @@
 package tasks.adts
 
-import u03.Sequences.*
+/*import u03.Sequences.Sequence
+import Sequence.map*/
 import u03.Optionals.*
+import u03.extensionmethods.Sequences.Sequence
+import u03.extensionmethods.Sequences.Sequence.*
+
+
 
 /*  Exercise 3: 
  *  Implement a Stack ADT
@@ -13,6 +18,8 @@ import u03.Optionals.*
  */
 object Ex3Stacks:
 
+  private case class StackType(s: Sequence[Int])
+
   trait StackADT:
     type Stack[A]
     def empty[A]: Stack[A] // factory
@@ -22,9 +29,9 @@ object Ex3Stacks:
       def asSequence(): Sequence[A]
 
   object StackImpl extends StackADT:
-    type Stack[A] = Nothing
-    def empty[A]: Stack[A] = ???
+    opaque type Stack[A] = StackType
+    def empty[A]: Stack[A] = StackType(nil())
     extension [A](stack: Stack[A])
       def push(a: A): Stack[A] = ???
       def pop(): Optional[(A, Stack[A])] = ???
-      def asSequence(): Sequence[A] = ???
+      def asSequence(): Sequence[A] = stack.s.map(a => a.asInstanceOf[A])
--- src/test/scala/tasks/adts/StackTest.scala
+++ src/test/scala/tasks/adts/StackTest.scala
@@ -3,8 +3,9 @@ package tasks.adts
 import org.junit.*
 import org.junit.Assert.*
 import tasks.adts.Ex3Stacks.StackImpl
-import u03.Sequences.Sequence
+import u03.extensionmethods.Sequences.Sequence.*
 import u03.Optionals.Optional
+import u03.extensionmethods.Sequences.Sequence
 
 /* Tests should be clear, but note they are expressed independently of the 
    specific implementation
